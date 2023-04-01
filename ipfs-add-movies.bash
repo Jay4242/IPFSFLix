8385ec82b4a9401fb2557acb66c751c2d24ebbdd
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2023-04-01 12:42:37 -0700

    Update ipfs-add-movies.bash
    Changing configuation settings.

--- ipfs-add-movies.bash
+++ ipfs-add-movies.bash
@@ -1,21 +1,26 @@
 #!/bin/bash
-confdir="/dev/null/ipfsflix/"   #CHANGE to directory to store the IPFS file list.
-
-#Find all .mp4 & .mkv in the current directory.
+if [ -e ~/.config/ipfsflix.conf ] ; then
+  source ~/.config/ipfsflix.conf || exit 1
+  if [ -z "${confdir}" ] ; then
+    echo "confdir not set correctly in config file at ~/.config/ipfsflix.conf"
+    exit 1
+  fi
+else
+  echo "Configuration not found at ~/.config/ipfsflix.conf"
+  exit 1
+fi
 mapfile -t movies < <(find $(pwd) \( -iname "*.mp4" -o -iname "*.mkv" \) -print | sort )
 
-#Loop through them and prompt to add to IPFS.
 for movie in "${movies[@]}" ; do
   echo "${movie}"
-  read -p "Add movie to IPFS? [Y/yes]/[No]: " answer
+  read -p "Add movie to IPFS? [Y/yes]: " answer
   if [[ "$answer" =~ [Yy] ]] ; then
-    dir=$(basename "$(echo "${movie%.*}")"| sed -e 's/ /./g')  #Create a directory name based on the name of the file.
-    if ipfs files ls /movies/ | grep "${dir}" ; then   #If the directory already exists, remove it.
-      if [ -n "${dir}" ] ; then  #Test if $dir is not null so we don't wipe out all of /movies/
+    dir=$(basename "$(echo "${movie%.*}")"| sed -e 's/ /./g')
+    if ipfs files ls /movies/ | grep "${dir}" ; then
+      if [ -n "${dir}" ] ; then
         ipfs files rm -rf "/movies/${dir}"
       fi
     fi
-  #Add the file to IPFS without copying the data into the block store.  Place it in a sub-directory under /movies/.  Log the file to the ipfs-filesystem.list.
-  cid=$(ipfs add -w --nocopy --to-files="/movies/${dir}" "${movie}" | tail -n 1 | sed -e 's/.* //g') && echo "${movie}::/movies/${dir}::${cid}::${IPFS_PATH}" >> ${confdir}ipfs-filesystem.list
+  cid=$(ipfs add -w --nocopy --to-files="/movies/${dir}" "${movie}" | tail -n 1 | awk -F' ' '{print $2}') && echo "${movie}::/movies/${dir}::${cid}::${IPFS_PATH}" >> ${confdir}ipfs-filesystem.list
   fi
 done

a29852bb4f4bffcb8d33b15144a36e5d9a919973
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2023-04-01 17:22:32 -0700

    Update ipfsflix-ipns-refresh.bash
    Fixes some misconceptions I had about bash and adds some config changes.  Timeout now gets set in the ~/.config/ipfsflix.conf

--- ipfsflix-ipns-refresh.bash
+++ ipfsflix-ipns-refresh.bash
@@ -1,34 +1,43 @@
 #!/bin/bash
-namemap="ipfs-namemap.list"             #Text file with relative/path::symlink-name::/ipns/address::IPFS_PATH for each IPNS address to resolve & symlink.
-symlinkdir="/dev/null/ipfs-media/"        #CHANGE THIS.  Base directory the symlinks will be located at, along with the above namemap.
-timeout="30"                           #Timeout for both IPNS resolve & 'tree' of the files & directories to fetch the structure.  Both can potentially take a long time.
+if [ -e ~/.config/ipfsflix.conf ] ; then
+  source ~/.config/ipfsflix.conf || exit 1
+  if [ -z "${confdir}" ] ; then
+    echo "confdir not set correctly in config file at ~/.config/ipfsflix.conf"
+    exit 1
+  fi
+  if [ -z "${symlinkdir}" ] ; then
+    echo "symlinkdir not set correctly in config file at ~/.config/ipfsflix.conf"
+    exit 1
+  fi
+  if [ -z "${timeout}" ] ; then
+    echo "timeout not set correctly in config file at ~/.config/ipfsflix.conf"
+    exit 1
+  fi
+else
+  echo "Configuration not found at ~/.config/ipfsflix.conf"
+  exit 1
+fi
 
-#cd to symlink base directory.
+set -x
+namemap="ipfsflix-namemap.txt"             #Text file with relative/path::symlink-name::/ipns/address::IPFS_PATH for each IPNS address to resolve & symlink.
+
 cd "${symlinkdir}" || exit 1
-
-#find any existing symlinks.
-mapfile -t links < <(find . -maxdepth 1 -type l | sed 's,^\./,,')
-
-#Check if symlinks exist in our list, remove them if they aren't.
+mapfile -t links < <(find "${symlinkdir}" -maxdepth 1 -type l | sed 's,^\./,,')
 for link in "${links[@]}" ; do
-  grep -q "${link}" "${namemap}" || rm "${link}"
+  grep -q "${link}" "${confdir}/ipfsflix-namemap.txt" || rm "${link}"
 done
-
-#Get IPNS symlink list.
-mapfile -t ipns < <(cat "${namemap}") || exit 1
-
-#Loop through IPNS symlink list.
+mapfile -t ipns < <(cat "${confdir}/ipfsflix-namemap.txt") || exit 1
 for ipn in "${ipns[@]}" ; do
-  export IPFS_PATH=$(echo "${ipn}" | awk -F'::' '{print $4}')  #Export correct IPFS_PATH
-  vname=$(echo "${ipn}" | awk -F'::' '{print $2 }')  #Find symlink name.
-  cid=$(ipfs name resolve --dhtt "${timeout}s" $(echo ${ipn} | awk -F'::' '{print $3}')) || echo "Failed to resolve for ${vname}" && continue  #Resolve the IPNS address to an IPFS address.  Move on if it takes longer than $timeout.
-  mcid=$(echo -n "$(echo ${ipn} | awk -F'::' '{print $1}')${cid}")  #Construct the symlink CID location.
-  if [[ -L "${vname}" ]] ; then  #If the symlink already exists check if it changed, if not we can skip it.
+  export IPFS_PATH=$(echo "${ipn}" | awk -F'::' '{print $4}')
+  vname=$(echo "${ipn}" | awk -F'::' '{print $2 }')
+  cid=$(ipfs name resolve --dhtt "${timeout}s" $(echo ${ipn} | awk -F'::' '{print $3}')) || continue
+  mcid=$(echo -n "$(echo ${ipn} | awk -F'::' '{print $1}')${cid}")
+  if [[ -L "${vname}" ]] ; then
     ecid=$(readlink "${vname}")
     if [[ "${ecid}" == "${mcid}" ]] ; then
       continue
     fi
   fi
-  ln -sf "${mcid}" "${vname}" || echo "Failed to make symlink ${vname} to ${mcid}" && exit 1  #Create or update the symlink with the new IPFS address.
-  timeout ${timeout}s tree "${vname}" > /dev/null  #Traverse the new IPFS directory with 'tree' to cache the file info.
+  ln -sf "${mcid}" "${vname}" || continue
+  timeout ${timeout}s tree "${vname}" > /dev/null
 done

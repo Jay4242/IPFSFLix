c372f11dac60545756afc1f553f13dffcb6fd806
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2023-04-01 18:02:00 -0700

    Update ipfsflix-publish.bash

--- ipfsflix-publish.bash
+++ ipfsflix-publish.bash
@@ -1,48 +1,74 @@
 #!/bin/bash
 let plexserv=0
 let jellyserv=0
-symlinkdir="/dev/null/"
+if [ -e ~/.config/ipfsflix.conf ] ; then
+  source ~/.config/ipfsflix.conf || exit 1
+  if [ -z "${confdir}" ] ; then
+    echo "confdir not set correctly in config file at ~/.config/ipfsflix.conf"
+    exit 1
+  fi
+else
+  echo "Configuration not found at ~/.config/ipfsflix.conf"
+  exit 1
+fi
 
-#If plex is running, remember that and stop plex.  (prevents using a file/directory while we change things)
 if sudo service plexmediaserver status | grep -i "(running)"
 then
    let plexserv=1
    sudo service plexmediaserver stop
 fi
 
-#If jellyfin is running, remember that and stop Jellyfin.  (prevents using a file/directory while we change things)
 if sudo service jellyfin status | grep -i "(running)"
 then
    let jellyserv=1
    sudo service jellyfin stop
 fi
 
-#Unmount IPFS/IPNS mounts.
+#Unmount IPFS/IPNS mounts.  IPNS cannot be published while mounted.
 mapfile -t mounts < <(mount | grep -i -e "/ipfs" -e "/ipns" | awk -F' ' '{print $3}')
 for mount in "${mounts[@]}" ; do
   sudo umount "${mount}"
 done
 
-#Publish the new 'movies' directory.
-ipfs name publish --key=movies "$(ipfs files stat --hash /movies)"
+#Store the paths in an array.
+mapfile -t paths < <(cat ${confdir}/ipfsflix-paths.list)
 
-#Re-mount the IPFS/IPNS mounts.
-mapfile -t paths < <(cat ${symlinkdir}ipfsflix-namemap.txt | awk -F'::' '{print $4}' | sort -u)
+#Loop through the paths.
 for path in "${paths[@]}" ; do
-  export IPFS_PATH=${path}
-  ipfs mount
+
+  #Export the path.
+  export IPFS_PATH="${path}"
+  #Store the path keys in an array.
+  mapfile -t keys < <(ipfs key list)
+
+  #Loop through the keys and check if there's a corresponding root directory.
+  for key in "${keys[@]}" ; do
+
+    #If the key matches a base directory.
+    if ipfs files ls | grep -i -q "${key}" ; then
+      ipfs name publish --key="${key}" "$(ipfs files stat --hash /${key})"
+    fi
+  done
 done
 
 #Refresh local symlinks to IPFS addresses.
 ipfsflix-ipns-refresh.bash
 
-#If plex was running before, start it again.
+
+#Re-mount the IPFS/IPNS mounts.
+mapfile -t paths < <(cat ${confdir}ipfsflix-paths.list)
+for path in "${paths[@]}" ; do
+  export IPFS_PATH=${path}
+  ipfs mount
+done
+
+#Restart Plex if it was running.
 if [[ "${plexserv}" == "1" ]]
 then
    sudo service plexmediaserver start
 fi
 
-#If jellyfin was running before, start it again.
+#Restart Jellyfin if it was running.
 if [[ "${jellyserv}" == "1" ]]
 then
    sudo service jellyfin start

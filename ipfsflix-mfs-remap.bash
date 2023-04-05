dbc6e1a2a51c7e1448019a6c755eb64fc9295ee4
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2023-04-04 19:05:07 -0700

    Create ipfsflix-mfs-remap.bash
    Because sometimes the MFS loses things...

---
+++ ipfsflix-mfs-remap.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Load configuration file.
+if [ -e ~/.config/ipfsflix.conf ] ; then
+  source ~/.config/ipfsflix.conf || echo "~/.config/ipfsflix.conf not sourced correctly"
+  if [ -z "${confdir}" ] ; then
+    echo "confdir not set correctly in config file at ~/.config/ipfsflix.conf"
+    exit 1
+  fi
+else
+  echo "Configuration not found at ~/.config/ipfsflix.conf"
+  exit 1
+fi
+
+#Get all IPFS_PATHs
+mapfile -t paths < <(cat ${confdir}/ipfsflix-paths.list)
+
+#Loop through each IPFS_PATH
+for path in "${paths[@]}" ; do
+
+  #Export the IPFS_PATH
+  export IPFS_PATH="${path}"
+
+  #Get all base directories.
+  mapfile -t dirs < <(ipfs files ls)
+
+  #Loop through each directory.
+  for dir in "${dirs[@]}" ; do
+
+    #For each directory & path, get all corresponding lines.
+    mapfile -t rows < <(cat ${confdir}/ipfsflix-filesystem.list | grep -i "${path}" | grep -i "${dir}" )
+
+    #Loop through each row that matched.
+    for row in "${rows[@]}" ; do
+
+      #Get the IPFS MFS Location.
+      mfs=$(echo "${row}" | awk -F'::' '{print $2}')
+
+      #Get the CID of the file we inserted to IPFSFLix.
+      cid=$(echo "${row}" | awk -F'::' '{print $3}')
+
+      #Remap the ${cid} to the ${mfs} location.
+      ipfs files cp "/ipfs/${cid}" "${mfs}"
+
+    done  #Row is done.
+
+  done  #Directory is done.
+
+done  #Path is done.

016e5f76ec2daa2db2ec913d714f1a3f80503971
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2023-04-09 17:17:54 -0700

    Update ipfsflix-mfs-remap.bash
    Adding support for lower directories for the addition of series.

--- ipfsflix-mfs-remap.bash
+++ ipfsflix-mfs-remap.bash
@@ -39,6 +39,18 @@ for path in "${paths[@]}" ; do
       #Get the CID of the file we inserted to IPFSFLix.
       cid=$(echo "${row}" | awk -F'::' '{print $3}')
 
+      #Check if directory structure exists.
+      unset dirpath
+      mapfile -t layers < <(echo "${mfs%/*}" | sed -e 's/\//\n/g' | grep -v "^$")
+      for layer in "${layers[@]}" ; do
+
+        dirpath="${dirpath}/${layer}"
+        if ! ipfs files stat "${dirpath}" &>/dev/null ; then
+           ipfs files mkdir "${dirpath}"
+        fi
+
+      done
+
       #Remap the ${cid} to the ${mfs} location.
       ipfs files cp "/ipfs/${cid}" "${mfs}"
 
